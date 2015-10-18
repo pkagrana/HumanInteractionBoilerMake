2702df2af477481f450c353a1d9114898637ab1e
pkagrana <super123pratik@gmail.com>
2015-10-18 02:56:12 -0400

    Latest Version (9:52 PM)

--- app/src/main/java/com/example/pratik/humaninteractionboilermake/Home.java
+++ app/src/main/java/com/example/pratik/humaninteractionboilermake/Home.java
@@ -4,6 +4,13 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.widget.ScrollView;
+import android.widget.LinearLayout;
+import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
 
 
 public class Home extends Activity {
@@ -34,5 +41,32 @@ public class Home extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(Home.this, UserEntersSurvey.class);
+        startActivity(intent);
+    }
+    public void addElement () {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.activity_home, null);
+
+        // Find the ScrollView
+        ScrollView sv = (ScrollView) v.findViewById(R.id.scrollView1);
+
+        // Create a LinearLayout element
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        // Add text
+        TextView tv = new TextView(this);
+        tv.setText("my text");
+        ll.addView(tv);
+
+        // Add the LinearLayout element to the ScrollView
+        sv.addView(ll);
+
+        // Display the view
+        setContentView(v);
     }
 }
